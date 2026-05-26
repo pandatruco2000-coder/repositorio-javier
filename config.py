deacf448d47857c69bae738caa00631a821e4366
javierramirez <pandatruco2000@gmail.com>
2026-05-26 18:02:55 -0400

    version 1

---
+++ config.py
@@ -0,0 +1,21 @@
+
+
+# CONSTANTES Y VARIABLES DE INICIO
+ANCHO, ALTO = 640, 480
+FPS = 60
+TITULO = 'Proyecto Shooter'
+BACK_COLOR = (145, 120, 177)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+GREEN = (0, 250, 50)
+YELLOW = (200, 250, 50)
+PLAYER_IMG = 'src/ship.png'
+ENEMY_IMG = 'src/aliens.png'
+ROCK_IMG = 'src/rock.png'
+BULLET_IMG = 'src/ship.png'
+BACKGROUND_IMG = 'src/fondo.jpg'
+VICTORY_IMG = ''
+GAMEOVER_IMG = 'src/gameover.jpg'
+BGM = 'src/bgm.mp3'
+FIRE_FX = 'src/laser.mp3'
+GAME_FONT = 'src/font.ttf'
